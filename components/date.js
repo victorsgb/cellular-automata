d2a45e31c91e5d217bbedfac6367ee0c6518c1b5
Victor Baptista <victorsgb@gmail.com>
2023-03-30 16:29:30 -0300

    Moving all relevant stuff  to root

---
+++ components/date.js
@@ -0,0 +1,8 @@
+// Core components
+import { parseISO, format } from 'date-fns';
+
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+}
